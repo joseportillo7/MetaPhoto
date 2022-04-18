b358c451d66ab0b4ebfee634f811847cc09f0f08
JosePortillo <jose.portillo.samayoa@gmail.com>
2022-04-17 21:37:38 -0600

    Creando el servidor para iniciar con el desarrollo del API

--- Servidor/index.js
+++ Servidor/index.js
@@ -1,1 +1,14 @@
-console.log("Welcome MetaPhoto Project");
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+app.use('/', (req, res) => {
+    res.json({
+        message: 'Hello MetaPhoto application'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto en http://localhost:${PORT}`);
+})
