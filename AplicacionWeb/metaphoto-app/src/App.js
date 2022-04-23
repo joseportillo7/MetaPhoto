65b1852db9b5800e7fd12ee5891a4efe7886888f
JosePortillo <jose.portillo.samayoa@gmail.com>
2022-04-23 00:41:25 -0600

    Se lee la data del API expuesta y se muestra en pantalla v1

--- AplicacionWeb/metaphoto-app/src/App.js
+++ AplicacionWeb/metaphoto-app/src/App.js
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react";
+
 function App() {
+
+ const [info, setInfo] = useState([]);
+
+    useEffect(() => {
+      
+        fetch("http://localhost:5000/api/MetaPhoto/photos?album.title=quidem")        
+        .then( response => response.json())
+        .then( data => {
+            console.log(data)
+            setInfo(data)           
+        })
+        .catch( error => console.log(error))
+    
+      return () => {
+      }
+    }, [])
+    
     return (
-        <h1> WEB APPLICATION METAPHOTO </h1>
+        <div>
+            {                                
+                info.arreglofiltrado.map((element) => ( <div>{element.title}</div>) )
+            }            
+        </div>
     );
 }
 
